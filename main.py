6d6e8893dd68cd68adca4e357c37bf672aabb4cf
frosted-cake1 <103012457+frosted-cake1@users.noreply.github.com>
2022-04-26 15:46:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import PIL.Image
+
+img_flag = True
+path = input("Enter image path here: \n")
+
+try:
+	img = PIL.Image.open(path)
+	img_flag = True
+except:
+	print(path, "unable to find img path ");
+
+width, height = img.size
+aspect_ratio = height/width
+new_width = 120
+new_height = aspect_ratio * new_width * 0.55
+img = img.resize((new_width, int(new_height)))
+
+img = img.convert('P')
+
+chars = ["^", "V", "~", "7", "I", "Q", "e", ";", "`", "B", ":"]
+
+pixels = img.getdata()
+new_pixels = [chars[pixel//25] for pixel in pixels]
+new_pixels = ''.join(new_pixels)
+new_pixels_count = len(new_pixels)
+ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
+ascii_image = "\n".join(ascii_image)
+
+with open("ascii_image.txt", "w") as f:
+	f.write(ascii_image)
